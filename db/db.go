9774a17f4b64fdf00c64e98ad6f9fcf537b95e61
shinnosuke-K <shin.050195@gmail.com>
2019-12-30 17:04:09 +0900

    test

--- db/db.go
+++ db/db.go
@@ -8,5 +8,5 @@ import (
 )
 
 func Open() (*gorm.DB, error) {
-	return gorm.Open("postgres", os.Getenv("DATABASE_URL")+"sslmode=disable")
+	return gorm.Open("postgres", os.Getenv("DATABASE_URL")+"?sslmode=disable")
 }
